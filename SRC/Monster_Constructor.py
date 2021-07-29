889b69237d340a6d95fcffbee69ad4dd1fb1e9f7
tom <tom@inmail.me>
2021-07-29 14:53:52 +0200

    changed __init__ to instance most necessary attributes

--- SRC/Monster_Constructor.py
+++ SRC/Monster_Constructor.py
@@ -11,17 +11,57 @@ os.chdir(os.path.split(os.getcwd())[0])
 
 # This is the parent class when building around CR
 class MonsterFromCR():
-    def __init__(self, CR, size="Medium", legendary=False):
+    def __init__(self, CR, size="Medium", legendary=False, name='Monster', alignment="Unaligned",
+                 type="Monstrosity", resistences=None, immunities=None, cond_immun=None, attacks=1):
         self.CR = CR
+        self.name = name
         self.legendary = legendary
+        self.size = size
+        self.type = type
+        self.alignment = alignment
+        self.resistences = resistences
+        self.immunities = immunities
+        self.condition_immunity = cond_immun
+        df = pd.read_csv("Data/size_hp_chart.csv", index_col=0)
+        self.hit_die = df.loc[size, 'Hit Die']
+        self.hp_avg = df.loc[size, 'Average HP per Die']
+        self.attack_num = attacks
+        del df
+    def set_type(self, type):
+        self.type = type
+    def set_CR(self, CR):
+        self.CR = CR
+    def set_size(self, size):
         self.size = size
         df = pd.read_csv("Data/size_hp_chart.csv", index_col=0)
         self.hit_die = df.loc[size, 'Hit Die']
         self.hp_avg = df.loc[size, 'Average HP per Die']
         del df
-    def type(self, type):
-        self.type = type
+    def name_change(self, name):
+        self.name = name
+    def set_alignment(self, alignment):
+        self.alignment = alignment
+    def set_resistence(self, resistence):
+        self.resistences = resistence
+    def set_immunity(self, immunity):
+        self.immunities = immunity
+    def set_condition_immunity(self, cond_immunity):
+        self.condition_immunity = cond_immunity
+    def show(self):
+        print("This is {name}, a CR{CR} {size} {type} of {alignment} alignment \n"
+              "Tt has these resistances: {resistances} \n"
+              "It has these immunities:  {immunities} \n"
+              "It has these condition immunities: {cond_immun} \n"
+              "Legendary Status: {legend}".format(name=self.name, CR = self.CR, size=self.size, type=self.type,
+                                                  alignment = self.alignment, resistances=self.resistences,
+                                                  immunities=self.immunities, legend = self.legendary,
+                                                  cond_immun=self.condition_immunity))
+
 
 
 test = MonsterFromCR(5, size="Gargantuan")
-print(test.hit_die)
+test.show()
+test.set_type("Humanoid")
+test.name_change("David")
+test.set_resistence(["Fire", "Cold"])
+test.show()
